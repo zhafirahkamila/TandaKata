fbe16ef45b11db669e3711a38602b841614430d6
KeenanAP <161949105+KeenanAP@users.noreply.github.com>
2025-05-08 10:44:30 +0700

    Update videotutorial.dart

--- lib/videotutorial.dart
+++ lib/videotutorial.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
-import 'package:flutter/src/material/icons.dart';
 
 class youtube_test extends StatefulWidget {
   const youtube_test({Key? key}) : super(key: key);
@@ -32,24 +31,34 @@ class _youtubeTest extends State<youtube_test> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: const Text('Sign Language Basic'),
-      centerTitle: true,
-      actions: const [
-        IconButton(onPressed: null, icon: Icon(Icons.backspace),
-        ),
-      ],
+      appBar: AppBar(
+        title: const Text('Sign Language Basic'),
+        centerTitle: true,
+        leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
       ),
-      body:Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-          YoutubePlayer(controller: _controller,
-          showVideoProgressIndicator: true,
-          onReady: () => debugPrint('Ready'),
-          ),
-          const Text('Tips how to learn sign language'),
-
-          Container(
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: YoutubePlayer(
+                controller: _controller,
+                showVideoProgressIndicator: true,
+                onReady: () => debugPrint('Ready'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Tips how to learn sign language',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Container(
               margin: const EdgeInsets.only(bottom: 16),
               padding: const EdgeInsets.all(12),
               decoration: BoxDecoration(
@@ -66,7 +75,8 @@ class _youtubeTest extends State<youtube_test> {
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: const Center(
-                      child: Icon(Icons.play_arrow, color: Colors.amber, size: 32),
+                      child:
+                          Icon(Icons.play_arrow, color: Colors.amber, size: 32),
                     ),
                   ),
                   const SizedBox(width: 16),
@@ -74,7 +84,8 @@ class _youtubeTest extends State<youtube_test> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Sign Language Basic : Alphabet", style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text("Sign Language Basic : Alphabet",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                         SizedBox(height: 4),
                         Text("3:07"),
                       ],
@@ -92,7 +103,7 @@ class _youtubeTest extends State<youtube_test> {
                 ],
               ),
             ),
-             Container(
+            Container(
               margin: const EdgeInsets.only(bottom: 16),
               padding: const EdgeInsets.all(12),
               decoration: BoxDecoration(
@@ -109,7 +120,8 @@ class _youtubeTest extends State<youtube_test> {
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: const Center(
-                      child: Icon(Icons.play_arrow, color: Colors.amber, size: 32),
+                      child:
+                          Icon(Icons.play_arrow, color: Colors.amber, size: 32),
                     ),
                   ),
                   const SizedBox(width: 16),
@@ -117,7 +129,8 @@ class _youtubeTest extends State<youtube_test> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Sign Language Basic : Alphabet", style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text("Sign Language Basic : Alphabet",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                         SizedBox(height: 4),
                         Text("3:07"),
                       ],
@@ -135,8 +148,7 @@ class _youtubeTest extends State<youtube_test> {
                 ],
               ),
             ),
-
-             Container(
+            Container(
               margin: const EdgeInsets.only(bottom: 16),
               padding: const EdgeInsets.all(12),
               decoration: BoxDecoration(
@@ -153,7 +165,8 @@ class _youtubeTest extends State<youtube_test> {
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: const Center(
-                      child: Icon(Icons.play_arrow, color: Colors.amber, size: 32),
+                      child:
+                          Icon(Icons.play_arrow, color: Colors.amber, size: 32),
                     ),
                   ),
                   const SizedBox(width: 16),
@@ -161,7 +174,8 @@ class _youtubeTest extends State<youtube_test> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Sign Language Basic : Alphabet", style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text("Sign Language Basic : Alphabet",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
                         SizedBox(height: 4),
                         Text("3:07"),
                       ],
@@ -179,7 +193,7 @@ class _youtubeTest extends State<youtube_test> {
                 ],
               ),
             ),
-        ],
+          ],
         ),
       ),
     );
